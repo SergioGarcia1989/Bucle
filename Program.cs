ea2ecec003b0aaad7ab07e9b301c5b95255abfbb
Sergi <sergioga56@gmail.com>
2017-09-12 15:47:36 +0200

    Bucle
    Line of asterisks

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Sergio Garcia Balsas
+// AutoWrite 6 asterisks using bucles
+using System;
+
+public class Bucle
+{
+    public static void Main()
+    {
+        int a = 1;
+
+        while (a <= 6)
+        {
+            Console.Write("*");
+            a = a + 1;
+        }
+        Console.WriteLine();
+    }
+}
